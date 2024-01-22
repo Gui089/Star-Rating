257f980725268cc35563ca844f05989f1d95e7fe
Guilherme <guilhermesilba811@gmail.com>
2024-01-22 19:46:48 -0300

    Star clicked rating add

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const containerStyle = {display : 'flex', gap: '1rem', alignItems: 'center'}
 const textStyle = { margin: 0 }
 const starContainerStyle = {
@@ -8,26 +10,34 @@ const starContainerStyle = {
   listStyle: 'none'
 };
 
-const starStyle = { width: '3rem', height: '3rem', display: 'block', cusor: 'pointer'};
+const starStyle = { width: '3rem', height: '3rem', display: 'block', cursor: 'pointer'};
 
-const Star = () => (
-  <li style={starStyle}>
-     <svg xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"> 
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-      </svg>
-  </li>
-)
+const Star = ({index, onRate}) => {
+ return (
+   <li onClick={() => onRate(index)} style={starStyle}>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 20 20"
+         fill="#000"
+         stroke="#000"> 
+        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+    </svg>
+   </li>
+  )
+}
+  
+
 
 const StarRating = ({ maxRating = 5 }) => {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = index => setRating(index + 1);
+
   return (
      <div style={containerStyle}>
        <ul style={starContainerStyle}>
-        {Array.from({length: maxRating }, (_, i) => <Star key={i} />)}
+        {Array.from({length: maxRating }, (_, i) => <Star onRate={handleRating} key={i} index={i} />)}
        </ul>
-       <p style={textStyle}>{maxRating}</p>
+       <p style={textStyle}>{rating || ''}</p>
      </div>
     );
 }
