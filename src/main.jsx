7a0783d65703faf8c3461fe882a556bf4f329e3f
Guilherme <guilhermesilba811@gmail.com>
2024-01-23 08:14:01 -0300

    Estrelas preenchidas ao dar nota

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { StarRating } from './StarRating.jsx'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StarRating />
